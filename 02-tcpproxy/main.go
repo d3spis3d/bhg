6d9e1690409b6dbb68c89bd277452e1c3830cd7f
Daniel Budden <daniel.l.budden@gmail.com>
2020-12-09 23:58:33 +1100

    Echo server, on way to TCP proxy

---
+++ 02-tcpproxy/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func echo(conn net.Conn) {
+	defer conn.Close()
+
+	b := make([]byte, 512)
+	for {
+		size, err := conn.Read(b[0:])
+		if err == io.EOF {
+			log.Println("Client disconnected")
+			break
+		}
+		if err != nil {
+			log.Println("Unexpected error")
+			break
+		}
+		log.Printf("Received %d bytes: %s\n", size, string(b))
+
+		log.Printf("Writing data")
+		if _, err := conn.Write(b[0:size]); err != nil {
+			log.Fatalln("Unable to write data")
+		}
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":20080")
+	if err != nil {
+		log.Fatalln("Unable to bind to port")
+	}
+	log.Println("Listening on port 20080")
+
+	for {
+		conn, err := listener.Accept()
+		log.Println("Received connection")
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+
+		go echo(conn)
+	}
+}
